eee86d8619869b442e916f9401169f489d4c740f
cannin <cannin@gmail.com>
2019-09-26 01:33:04 +0200

    Updated modified function and notebook

--- modified_clusterProfiler_functions.R
+++ modified_clusterProfiler_functions.R
@@ -77,23 +77,33 @@ emapplotModGraph <- function(x, showCategory = 30, color="p.adjust", layout = "k
   return(g)
 }
 
-emapplotModPlot <- function(g, limits, color="p.adjust", layout="kk", componentHighlight=1, tags="tags") {
+emapplotModPlot <- function(g, limits, color="p.adjust", layout="nicely", tags=NULL) {
   p <- ggraph(g, layout=layout)
   
   if (length(E(g)$width) > 0) {
-    p <- p + geom_edge_link(alpha=.8, aes_(width=~I(width)), colour='darkgrey')
+    p <- p + geom_edge_link(alpha=0.8, aes_(width=~I(width)), colour='darkgrey')
   }
   
-  p + geom_node_point(aes_(color=~color, size=~size, stroke=1)) +
-    
-    #geom_node_text(aes_(label=~name), repel=TRUE) + 
-    #geom_node_text(aes_(label=~component), repel=TRUE) + 
-    geom_mark_hull(con.cap = 0, concavity=20, aes(x, y, label=tags[1], filter=component %in% componentHighlight[1])) +
-    geom_mark_hull(con.cap = 0, concavity=20, aes(x, y, label=tags[2], filter=component %in% componentHighlight[2])) +
-    geom_mark_hull(con.cap = 0, concavity=20, aes(x, y, label=tags[3], filter=component %in% componentHighlight[3])) +
-    # geom_mark_hull(concavity=20, color="white", aes(x, y, label=tags) +
-    theme_void() +
-    scale_color_continuous(low="red", high="white", limits=limits, name=color, guide=guide_colorbar()) +
-    ## scale_color_gradientn(name = color, colors=sig_palette, guide=guide_colorbar(reverse=TRUE)) +
-    scale_size(range=c(3, 8))
+  p <- p + geom_node_point(aes(fill=color, size=size), color="black", shape=21, stroke=1)
+  #p <- p + geom_node_point(aes(fill=color, size=size), stroke=2)
+  p <- p + theme_graph()
+  p <- p + scale_fill_gradient2(low="cyan", mid="white", high="magenta", midpoint=0,
+                         limits=limits,
+                         breaks= c(min(limits), max(limits)),
+                         labels= c("ERBB2 Lo", "ERBB2 Hi"),
+                         name="Enrichment Score")
+  p <- p + scale_size(range=c(3, 12), name="Pathway Size")
+   
+  #   #geom_node_text(aes_(label=~name), repel=TRUE) + 
+  #   #geom_node_text(aes_(label=~component), repel=TRUE) + 
+ 
+  if(!is.null(tags)) {
+    p <- p + lapply(1:nrow(tags), function(i) {
+      #cat("X: ", a, " Y: ", b, "\n")
+      geom_mark_hull(aes(x, y, label=tags[i, "tag"], filter=component == tags[i, "component"]), 
+                     con.cap = 0, concavity=20)
+    })
+  }
+   
+  return(p)
 }
